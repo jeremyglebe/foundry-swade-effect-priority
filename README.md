d6600294e60bb71a6dd90c9b9f1ea328a4980e7c
Jeremy Glebe <jeremyglebe@gmail.com>
2024-06-09 21:15:40 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Module Manifest (for installing): [https://github.com/jeremyglebe/foundry-swade-effect-priority/releases/latest/download/module.json](https://github.com/jeremyglebe/foundry-swade-effect-priority/releases/latest/download/module.json)
---
+++ src/js/module.js
@@ -0,0 +1,27 @@
+import * as jsonData from '../module.json' assert { type: 'json' };
+const moduleId = jsonData.id;
+
+function priorityEditActiveEffectConfig(aeConfigApp, jqHtml, aeData) {
+  console.log(`${moduleId} | Hook: renderActiveEffectConfig`);
+  // Extract the effect object being modified
+  const effect = aeData.effect;
+
+  // Add a "priority" column to the header of the effects list (class "effects-header")
+  const header = jqHtml.find('.effects-header');
+  const priorityHeader = $('<div class="priority">Priority</div>');
+  header.append(priorityHeader);
+
+  // Find the HTML element with class "changes-list"
+  const changesList = jqHtml.find('.changes-list');
+  // Modify each of the list items in the changes list
+  changesList.children().each((index, li) => {
+    // Add a div with class "priority" to the list item
+    // It will be similar to the "value" div which contains <input type="text" name="changes.0.value" value="-1">
+    // This will be used to store the priority value
+    const priorityDiv = $(
+      `<div class="priority"><input type="text" name="changes.${index}.priority" value="${effect.changes[index].priority}"></div>`,
+    );
+    $(li).append(priorityDiv);
+  });
+}
+Hooks.on('renderActiveEffectConfig', priorityEditActiveEffectConfig);
