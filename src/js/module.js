552ed50766e44f6dd737859e53184388fa7cd398
Jeremy Glebe <jeremyglebe@gmail.com>
2024-08-04 23:07:04 -0500

    fixed effect priority showing as "null", "default is now the keyword for non-adjusted priorities

--- src/js/module.js
+++ src/js/module.js
@@ -15,13 +15,23 @@ function priorityEditActiveEffectConfig(aeConfigApp, jqHtml, aeData) {
   const changesList = jqHtml.find('.changes-list');
   // Modify each of the list items in the changes list
   changesList.children().each((index, li) => {
+    const existingPriorityValue = effect.changes[index].priority ?? 'default';
     // Add a div with class "priority" to the list item
     // It will be similar to the "value" div which contains <input type="text" name="changes.0.value" value="-1">
     // This will be used to store the priority value
     const priorityDiv = $(
-      `<div class="priority"><input type="text" name="changes.${index}.priority" value="${effect.changes[index].priority}"></div>`,
+      `<div class="priority"><input type="text" name="changes.${index}.priority" value="${existingPriorityValue}"></div>`,
     );
     $(li).append(priorityDiv);
   });
 }
 Hooks.on('renderActiveEffectConfig', priorityEditActiveEffectConfig);
+
+Hooks.on('preUpdateActiveEffect', (effect, updateData, options, userId) => {
+  const { changes } = updateData;
+  for (const change of changes) {
+    if (isNaN(change.priority)) {
+      change.priority = undefined;
+    }
+  }
+});
